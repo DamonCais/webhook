7057d5b0b73f0ebbe99dd26a07313371f5994ecd
DamonCais <516181085@qq.com>
2019-02-18 10:15:17 +0800

    test

---
+++ app.js
@@ -0,0 +1,36 @@
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser')
+const exec = require('child_process').exec;
+
+const app = new Koa()
+app.use(bodyParser());
+
+
+app.use(async (ctx, next) => {
+    console.log(ctx.request);
+
+    try {
+        let res = await mkfile();
+        ctx.body = res;
+
+    } catch (error) {
+        ctx.body = error;
+    }
+
+
+})
+app.listen(8082);
+
+function mkfile() {
+    return new Promise((resolve, reject) => {
+        const commands = 'sh ../update.sh';
+        exec(commands, (err, stdout, stderr) => {
+            if (err) {
+                reject(err);
+            }
+            console.log(stdout);
+            console.log(stderr);
+            resolve('success');
+        })
+    })
+}
