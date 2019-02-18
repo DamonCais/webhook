ad410e2a16d7a6768e97b93f809775c28f32a15e
DamonCais <516181085@qq.com>
2019-02-18 09:04:19 +0800

    test hook

--- serve/app.js
+++ serve/app.js
@@ -1,11 +1,34 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser')
+const exec = require('child_process').exec;
+
 const app = new Koa()
 app.use(bodyParser());
 
+
 app.use(async (ctx, next) => {
-    console.log(ctx.request.body);
-    ctx.body = 'success';
+    console.log(ctx.request);
+
+    try {
+        let res = await mkfile();
+        ctx.body = res;
+
+    } catch (error) {
+        ctx.body = error;
+    }
+
 
 })
 app.listen(8082);
+
+function mkfile() {
+    return new Promise((resolve, reject) => {
+        const commands = 'sh ./update.sh';
+        exec(commands, (err, out, code) => {
+            if (err) {
+                reject(err);
+            }
+            resolve('success');
+        })
+    })
+}
