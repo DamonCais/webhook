a2d8923891b749a3e9ae566885109063266120f8
DamonCais <516181085@qq.com>
2019-02-18 10:17:34 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome ssssssssss to Your Vue.js App"/>
+    <HelloWorld msg="Welcome aaa to Your Vue.js App"/>
   </div>
 </template>
 
